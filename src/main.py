aae90a608e7b2931ea26b89a486a65859b2c81e3
oriana calderon <orianasofia86@gmail.com>
2022-08-28 23:58:29 -0400

    falta la ruta post y delete

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
+from crypt import methods
 import os
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
@@ -8,12 +9,14 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, People, Planet, Favorites
+# from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
+db_url = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
+    "postgres://", 'postgresql://')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db)
 db.init_app(app)
@@ -21,25 +24,75 @@ CORS(app)
 setup_admin(app)
 
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+# @app.route('/user', methods=['GET'])
+# def handle_hello():
 
-    return jsonify(response_body), 200
+#     response_body = {
+#         "msg": "Hello, this is your GET /user response "
+#     }
+
+#     return jsonify(response_body), 200
+
+
+#ruta GET para todos los personajes y para traer personaje por id
+@app.route('/people', methods=['GET'])
+@app.route('/people/<int:people_id>', methods=['GET'])
+def handle_people(people_id=None):
+    if request.method == 'GET':
+        if people_id == None:
+            people = People()
+            people = people.query.all()
+            return jsonify(list(map(lambda item: item.serialize(), people)), 200)
+        else:
+            people = People()
+            people = people.query.get(people_id)
+            if people:
+                return jsonify(people.serialize())
+
+        return jsonify({"Not found"}), 404
+
+
+#ruta GET para traer todos los usuarios y para traer los favoritos de el usuario por su id
+@app.route('/users', methods=['GET'])
+@app.route('/users/<int:user_id>/favorite', methods=['GET'])
+def handle_user(user_id=None):
+    if request.method == 'GET':
+        if user_id == None:
+            user = User()
+            user = user.query.all()
+            
+            return jsonify(list(map(lambda item: item.serialize(), user)), 200)
+        else: 
+            favorites = Favorites()
+            favorites = favorites.query.all()
+            return jsonify(list(map(lambda items: items.serialize(), favorites)), 200)
+
+    return jsonify({"Not found"}),404
+
+
+#ruta para hacer POST de un favorito
+
+#ruta para hacer DELETE de un favorito
+
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
+https://github.com/OrianaCalderon/api-star_wars
--- src/models.py
+++ src/models.py
@@ -1,12 +1,21 @@
 from flask_sqlalchemy import SQLAlchemy
+from enum import Enum, unique
 
 db = SQLAlchemy()
 
+class Nature(Enum):
+    people="people",
+    planets="planets"
+
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(250), nullable=False)
+    lastname:db.Column(db.String(250), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password=db.Column(db.String(100), nullable=False)
+
+    favorites = db.relationship("Favorites", backref="user", uselist=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -14,6 +23,72 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "name":self.name,
+            "lastname": self.lastname,
+            "email": self.email
         }
+
+class Favorites(db.Model):
+    id= db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(250), nullable=False)
+    nature=db.Column(db.Enum(Nature), nullable=False)
+
+    nature_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "user_id": self.user_id,
+            "name": self.name,
+            "nature": self.nature
+        }
+
+
+class People(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(100), nullable=False, unique=True)
+    gender=db.Column(db.String(100), nullable=False)
+    birth_year=db.Column(db.String(100), nullable=False)
+    age=db.Column(db.String(100), nullable=False)
+    height=db.Column(db.String(100), nullable=False)
+    eye_color=db.Column(db.String(100), nullable=False)
+    hair_color=db.Column(db.String(100), nullable=False)
+    skin_color=db.Column(db.String(100), nullable=False)
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name": self.name,
+            "gender":self.gender,
+            "birth_year": self.birth_year,
+            "age":self.age,
+            "height":self.height,
+            "eye_color":self.eye_color,
+            "hair_color":self.hair_color,
+            "skin_color":self.skin_color
+        }
+
+
+
+class Planet(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(100), nullable=False, unique=True)
+    climate=db.Column(db.String(100), nullable=False)
+    population=db.Column(db.String(100), nullable=False)
+    orbital_period=db.Column(db.String(100), nullable=False)
+    rotation_period=db.Column(db.String(100), nullable=False)
+    diameter=db.Column(db.String(100), nullable=False)
+
+
+    def serialize(self):
+        return{
+            "id":self.id,
+            "name":self.name,
+            "climate":self.climate,
+            "population":self.population,
+           "orbital_period":self.orbital_period,
+           "rotation_period":self.rotation_period,
+           "diameter":self.diameter
+        }
+
